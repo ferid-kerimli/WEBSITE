7c220f8a0093e908d0d9c6e472144632855fd002
Abulfat-R <abulfatrzayev20@gmail.com>
2025-04-28 17:58:13 +0400

    Commit

---
+++ admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+console.log('▶ Starting admin.js');
+app.get('/login.html', (req, res) => {
+  console.log('Got request for /login.html, headers:', req.headers);
+
+  res.sendFile(path.join(__dirname,'downtown', 'adminpanel', 'login.html'), err => {
+    if (err) console.error('sendFile error:', err);
+    else    console.log('login.html sent ✔️');
+  });
+});
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`✅ listening on :${PORT}`));
