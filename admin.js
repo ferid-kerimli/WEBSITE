203f990bb1a72d2f2e10eb81921eb32305ba9ec8
Abulfat-R <abulfatrzayev20@gmail.com>
2025-04-28 18:33:45 +0400

    upd

--- admin.js
+++ admin.js
@@ -2,13 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 console.log('▶ Starting admin.js');
+app.use(express.static(path.join(__dirname, '/downtown/images/adminpanelimages'))); 
 app.get('/login.html', (req, res) => {
-  console.log('Got request for /login.html, headers:', req.headers);
-
   res.sendFile(path.join(__dirname,'login.html'), err => {
     if (err) console.error('sendFile error:', err);
     else    console.log('login.html sent ✔️');
   });
 });
+app.get('/mainpage.html', (req, res) => {
+  res.sendFile(path.join(__dirname,'downtown','adminpanel','mainpage.html'), err => {
+    if (err) console.error('sendFile error:', err);
+    else    console.log('mainpage.html sent ✔️');
+  });
+});
+app.post('/login', (req, res) => {
+  return res.redirect('/mainpage.html');
+}); 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`✅ listening on :${PORT}`));
