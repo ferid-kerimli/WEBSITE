bb90353030bd245b169585d1d79b7bd06005dff5
Abulfat-R <abulfatrzayev20@gmail.com>
2025-05-06 14:05:37 +0400

    p

---
+++ api/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+console.log('▶ Starting admin.js');
+
+app.use(session({
+  secret: 'pick-a-very-secret-string',
+  resave:   false,
+  saveUninitialized: false
+}));
+
+app.use(express.urlencoded({ extended: false }));
+app.get('/login', (req, res, next) => {
+  if (req.session.authenticated) {
+    return res.sendFile(path.join(__dirname,'..','downtown','adminpanel', 'mainpage.html'));
+  }
+  res.sendFile(path.join(__dirname,'..','downtown','adminpanel', 'login.html'));
+});
+app.post('/login', (req, res) => {
+  const {username,password}=req.body;
+  const ALLOWED_USER = 'kerim';
+  const ALLOWED_PASS = 'kerim123';
+
+  if (username === ALLOWED_USER && password === ALLOWED_PASS) {
+    req.session.authenticated = true;
+    return res.sendFile(path.join(__dirname,'..', 'downtown','adminpanel', 'mainpage.html'));
+  }
+  res.redirect('/login?error=1');
+});
+function requireAuth(req, res, next) {
+  if (!req.session.authenticated) {
+    return res.redirect('/login');
+  }
+  next();
+}
+
+app.get(
+  ['/mainpage.html','/mainpageimg.html','/portf.html','/decor.html','/info.html'],
+  requireAuth,
+  (req, res) => {
+    const name = req.path.replace(/^\//, '');
+    res.sendFile(path.join(__dirname,'..', 'downtown','adminpanel', name));
+    }
+);
+
+app.get('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) return next(err);
+    res.clearCookie('connect.sid');
+    res.redirect('/login');
+    });
+});
+app.use('/', express.static(__dirname));
+app.use(
+  '/downtown/images/adminpanelimages',
+  express.static(
+    path.join(__dirname, '..', 'images', 'adminpanelimages')
+  )
+);
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`✅ listening on :${PORT}`));
